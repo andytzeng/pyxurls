4c3cd9cc5f08348b6f30df1c868d5c024dadf107
Andy Tzeng <andy_tzeng@trendmicro.com>
2020-12-27 12:41:57 +0800

    Bump v0.1.1 for update readme

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='pyxurls',
-    version='0.1.0',
+    version='0.1.1',
     author='Andy Tzeng',
     author_email='andytzeng@aol.tw',
     description='A regular expression based URL extractor which extracts URLs from text.',
